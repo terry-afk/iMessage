1099f494a7fa769157df58bf5ac3cd481d01cfd7
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2021-06-28 02:39:17 +0200

    [ADD] sidebar

---
+++ src/Imessage/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./index.css"
+import Sidebar from "../Sidebar"
+
+function Imessage() {
+    return <div className="imessage">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Chat */}
+    </div>
+}
+
+export default Imessage;
---
+++ src/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Avatar, IconButton } from "@material-ui/core"
+import "./index.css"
+import SearchIcon from "@material-ui/icons/Search"
+import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined"
+import SidebarChat from "../SidebarChat"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar className="sidebar__avatar"/>
+                <div className="sidebar__input">
+                    <SearchIcon />
+                    <input placeholder="Search" />
+                </div>
+
+                <IconButton variant="outlined" className="sidebar__inputButton">
+                    <RateReviewOutlinedIcon />
+                </IconButton>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+            </div>
+
+        </div>
+    );
+}
+
+export default Sidebar;
